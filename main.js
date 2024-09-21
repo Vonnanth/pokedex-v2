1ed6671bbc8d4a53708cc1e3e86dc66c9c5b3936
Vonnanth <cryskawaiii@gmail.com>
2024-09-20 21:28:50 -0300

    Second commit

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-// Como só existe algumas cores de pokemons, usarei essa função que retorna as cores geradas por IA
-const get_color_pokemon = () => {
-    return [
-        '#61ca56', '#0be12d', '#e4e92d', '#aa7b31', '#9cbb5d',
-        '#a3e3d6', '#c2255b', '#3551b9', '#eb9d1c', '#015aa2',
-        '#64c353', '#f6a05e', '#50964d', '#5f14e9', '#639061',
-        '#0d28d0', '#595a12', '#db7ecf', '#abb85b', '#326aeb',
-        '#192f7d', '#2c8a8d', '#e2f6b8', '#d2579f', '#a540c3',
-        '#8beabf', '#0cdb41', '#898cde', '#58520b', '#1ec40f',
-        '#b4f12d', '#ed5edc', '#d30cb7', '#95e019', '#fc6bea',
-        '#b141a7', '#61904e', '#fc3568', '#7e458b', '#fbe320',
-        '#42389c', '#5dff57', '#ed742d', '#1fd2aa', '#f9a468',
-        '#0e0196', '#820221', '#139b16', '#8c9e38', '#a4046d',
-        '#ea84ca', '#603964', '#932f98', '#a545c5', '#81fb86',
-        '#67927e', '#64f535', '#907c34', '#f5905b', '#e95450',
-        '#8177cc', '#08132b', '#fdfc16', '#0aa552', '#89b620',
-        '#a2a3cb', '#695ff3', '#7f1a1e', '#b56ba8', '#76f629',
-        '#e7c3af', '#ab214b', '#27e9f4', '#1da3c7', '#dbc99b',
-        '#1343a9', '#16e855', '#ce9ddc', '#07c14f', '#bf366d',
-        '#1cd078', '#6df614', '#ea46ef', '#4ef75e', '#94fedc',
-        '#18627b', '#f496df', '#8f6660', '#31a15a', '#43767b',
-        '#dc3681', '#6d5970', '#2e79b7', '#e271bd', '#43bfeb',
-        '#0a6e6e', '#cb5684', '#c03f37', '#f305c1', '#3f83cf'
-    ]
-}
-
 const get_pokemon_info = async (id) => {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
 
@@ -48,13 +22,11 @@ const get_pokemons_data = async (limit, offset) => {
     let pokemons_data = [];
     for (let i = offset+1; i < limit+1; i++) {
         const poke_info =  await get_pokemon_info(i);
-        // const poke_desc = await get_pokemon_desc(i);
 
         pokemons_data[i] = {
             name: poke_info.name,
             color: get_color_pokemon()[i],
             info: poke_info,
-            // descriptions: poke_desc
         }
     }
 
@@ -129,17 +101,16 @@ const get_pokemon_details = async (id) => {
 
 const observar_elemento = (limit, offset) => {
     const observar = () => {
-        let target = $(`#list-pokemons li:last`); // Pega o último elemento da lista
+        let target = $(`#list-pokemons li:last`);
 
         if (target.length === 0) {
             return;
         }
 
-        let scrollPosition = $(window).scrollTop(); // Posição atual do scroll
-        let windowHeight = $(window).height(); // Altura da janela (viewport)
-        let elementPosition = target.offset().top; // Posição do elemento em relação ao topo da página
+        let scrollPosition = $(window).scrollTop();
+        let windowHeight = $(window).height();
+        let elementPosition = target.offset().top;
 
-        // Verifica se o elemento está visível na viewport
         if (scrollPosition + windowHeight > elementPosition) {
             limit += 12;
             offset += 12;
@@ -147,17 +118,14 @@ const observar_elemento = (limit, offset) => {
 
             target = $(`#poke-${limit}`);
             
-            // Remove o event listener do scroll para evitar execuções repetidas
             $(window).off('scroll', observar);
 
-            // Adiciona o listener de novo para o novo elemento
             setTimeout(() => {
-                observar_elemento(limit, offset); // Chama a função novamente para observar o próximo elemento
-            }, 1000); // Atraso opcional, para evitar execuções rápidas demais
+                observar_elemento(limit, offset)
+            }, 1000);
         }
     };
 
-    // Adiciona o event listener para o scroll
     $(window).on('scroll', observar);
 }
 
